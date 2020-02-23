038992fb709fb106ee5104e50f4e5c17f47dcf78
Abhishek <abhisinghrulz@gmail.com>
2020-02-23 22:49:19 +0530

    Adding Security Project

---
+++ FdaSecurity/src/main/java/com/fda/security/FdaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.fda.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdaSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdaSecurityApplication.class, args);
+	}
+
+}
